f38578ab3786c6e9c6e3d0dbb4d70ff32406df94
afton downey and matt knutson <aaike001@gmail.com>
2016-04-04 09:17:37 -0700

    added helpers and computed property for location

---
+++ app/routes/index.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      cities: this.store.findAll('city'),
+      rentals: this.store.findAll('rental')
+    });
+  },
+
+  actions: {
+    saveCity(params) {
+      var newCity = this.store.createRecord('city', params);
+      newCity.save();
+      this.transitionTo('index');
+    },
+  }
+});
---
+++ tests/unit/routes/community-board-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:community-board', 'Unit | Route | community board', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
